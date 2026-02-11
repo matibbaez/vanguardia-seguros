edb049051e019d2af5eca6eded159e0fe3da9e04
matibbaez <matiascaneda3@gmail.com>
2026-02-11 09:29:24 -0300

    Cambios en el footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; 
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://vanguardiaseguros.com.ar', 
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
